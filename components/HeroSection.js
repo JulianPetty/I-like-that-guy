eb46f5679347481a15027a63b660e4736c54a0fc
Julian <julianpetty02@gmail.com>
2025-02-11 13:43:26 -0600

    Improve hero section and blog card responsiveness

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,4 +1,3 @@
-
 'use client'; 
 
 import React from 'react';
@@ -42,8 +41,10 @@ export default function HeroSection({ postsWithPreview }) {
                             <div className="absolute inset-0 bg-black opacity-50"></div>
                             <div className="mx-auto w-[75vw] h-full">
                                 <div className="relative z-10 flex flex-col items-start justify-end w-full h-full text-orange-50 pb-8 px-4 sm:px-8 lg:px-0">
-                                    <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-md mb-4">{post.title}</h1>
-                                    <p className="text-sm sm:text-md md:text-lg mb-3 md:mb-6 max-w-xl text-[#e9e8e6]">{post.preview}</p>
+                                    <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-md mb-4 mt-20 sm:mt-0">{post.title}</h1>
+                                    <p className="hidden md:block text-sm sm:text-md md:text-lg mb-3 md:mb-6 max-w-xl text-[#e9e8e6]">
+                                        {post.preview}
+                                    </p>
                                     <a
                                         href={`/posts/${post.slug}`}
                                         className="text-lg text-[#e9e8e6] font-medium hover:text-orange-500 transition underline underline-offset-1"
